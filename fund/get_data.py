a2ce42ea9b980303112df60294f3a04128f556e5
qwt <wentao.qin@cienet.com.cn>
2018-09-24 22:27:40 +0800

    Add some funds

--- fund/get_data.py
+++ fund/get_data.py
@@ -1,35 +1,36 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from urllib import request
-from bs4 import BeautifulSoup
+from urllib import urlopen
 import re
 import logging
 import json
 
-result = request.urlopen('http://fundgz.1234567.com.cn/js/050002.js?').read().decode("utf-8")
-#result = urlopen('http://fund.eastmoney.com/pingzhongdata/004473.js?').read()
 
-result = eval(re.search('{.*}', result).group())
-logging.critical(result)
+#沪深300 中证500 050002 001052
 
-with open('result.json', 'w') as f:
-    f.write(json.dumps(result, ensure_ascii=False))
+def get_fund_data(fund_list):
+    """Get the data of fund.
 
+    :param fund_list: a list contains the codes of fund
+    :return: 0 or file result.json
+    """
+    if not isinstance(fund_list, list):
+        logging.error('Parameter fund_list is not a list')
+        return 0
+    result_all = {}
+    for i in fund_list:
+        try:
+            result = urlopen('http://fundgz.1234567.com.cn/js/{0}.js?'.format(i)).read().decode("utf-8")
+        except Exception, e:
+            logging.error(e)
+        else:
+            result = eval(re.search('{.*}', result).group())
+            result_all[result['name']] = result
 
+    with open('result.json', 'w') as f:
+        f.write(json.dumps(result_all, ensure_ascii=False))
 
+fund_list = ['050002', '001052', '161725', '270042', '110011']
+get_fund_data(fund_list)
 
-
-
-
-
-
-
-# soup = BeautifulSoup(result, 'html.parser', from_encoding = 'utf-8')
-# for i in soup.find_all('td', class_='alignLeft'):
-#     print i.get_text()
-#     #print i.parent
-#     try:
-#         print i.next_sibling().get_text()
-#     except:
-#         pass
---
+++ fund/parse_data.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pandas as pd
+import numpy as np
+
+
+
